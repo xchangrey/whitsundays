3a479eb3e390854e74216808f65ac93ea2306906
Christian Geroy <geroychristian@gmail.com>
2018-09-09 12:40:51 +0800

    Added content and css  on the solutions and results sections

--- dist/main.js
+++ dist/main.js
@@ -29,7 +29,23 @@ $(document).ready(function() {
 
     })
 
+
+    //Tabbed content
+    $('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('active');
+		$('.tab-content').removeClass('active');
+
+		$(this).addClass('active');
+		$("#"+tab_id).addClass('active');
+	})
+
 })
+
+
+
+
 
 
 
